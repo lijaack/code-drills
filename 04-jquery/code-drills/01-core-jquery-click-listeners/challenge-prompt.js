89c97e98c11e9bf1f99ebabf4a9a7d7e8ec0f9b1
Jack <lijaack@users.noreply.github.com>
2018-11-17 19:52:37 -0800

    some code drills done

---
+++ 04-jquery/code-drills/01-core-jquery-click-listeners/challenge-prompt.js
@@ -0,0 +1,106 @@
+
+
+// document.ready makes sure the javascript doesn't load before the html page
+$(document).ready(function () {
+
+    // Write a series of on click listeners for each letter in the html
+    // Whenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
+    // You will need to set up an on click listener for each letter
+    //
+    // Within the document, set an on click listener for the element with a class of "letterA"
+
+    
+        // When that element is clicked, append the string "A" to the `text-div` element
+
+        
+    // Within the document, set an on click listener for the element with a class of "letterB"
+
+    
+        // When that element is clicked, append the string "B" to the `text-div` element
+
+        
+    // Continue for the other letters
+    $(".letterA").click(function(){
+        console.log("hello")
+        $("#text-div").append("A");
+    });
+    $(".letterB").click(function(){
+        $("#text-div").append("B");
+    });
+    $(".letterC").click(function(){
+        $("#text-div").append("C");
+    });
+    $(".letterD").click(function(){
+        $("#text-div").append("D");
+    });
+    $(".letterE").click(function(){
+        $("#text-div").append("E");
+    });
+    $(".letterF").click(function(){
+        $("#text-div").append("F");
+    });
+    $(".letterG").click(function(){
+        $("#text-div").append("G");
+    });
+    $(".letterH").click(function(){
+        $("#text-div").append("H");
+    });
+    $(".letterI").click(function(){
+        $("#text-div").append("I");
+    });
+    $(".letterJ").click(function(){
+        $("#text-div").append("J");
+    });
+    $(".letterK").click(function(){
+        $("#text-div").append("K");
+    });
+    $(".letterL").click(function(){
+        $("#text-div").append("L");
+    });
+    $(".letterM").click(function(){
+        $("#text-div").append("M");
+    });
+    $(".letterN").click(function(){
+        $("#text-div").append("N");
+    });
+    $(".letterO").click(function(){
+        $("#text-div").append("O");
+    });
+    $(".letterP").click(function(){
+        $("#text-div").append("P");
+    });
+    $(".letterQ").click(function(){
+        $("#text-div").append("Q");
+    });
+    $(".letterR").click(function(){
+        $("#text-div").append("R");
+    });
+    $(".letterS").click(function(){
+        $("#text-div").append("S");
+    });
+    $(".letterT").click(function(){
+        $("#text-div").append("T");
+    });
+    $(".letterU").click(function(){
+        $("#text-div").append("U");
+    });
+    $(".letterV").click(function(){
+        $("#text-div").append("V");
+    });
+    $(".letterW").click(function(){
+        $("#text-div").append("W");
+    });
+    $(".letterX").click(function(){
+        $("#text-div").append("X");
+    });
+    $(".letterY").click(function(){
+        $("#text-div").append("Y");
+    });
+    $(".letterZ").click(function(){
+        $("#text-div").append("Z");
+    });
+
+
+
+        
+})
