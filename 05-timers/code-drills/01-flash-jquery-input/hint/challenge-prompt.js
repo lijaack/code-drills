0b65f772bf2c9fb7430d919521a388aab0ec345c
Jack <lijaack@users.noreply.github.com>
2018-11-24 21:25:08 -0800

    timer-01 done

--- 05-timers/code-drills/01-flash-jquery-input/hint/challenge-prompt.js
+++ 05-timers/code-drills/01-flash-jquery-input/hint/challenge-prompt.js
@@ -8,7 +8,27 @@ $(document).ready(function(){
 
 
   // Create a click listener for the submit button
-  
+  $("#submit-button").on("click", function(){
+    event.preventDefault();
+    var firstName=$("#first-name").val();
+    var lastName = $("#last-name").val();
+    var occupation = $("#occupation").val();
+    var city = $("#city").val();
+    var state = $("#state").val();
+
+    $("#first-name").val("");
+    $("#last-name").val("");
+    $("#occupation").val("");
+    $("#city").val("");
+    $("#state").val("");
+
+    var name = $("<p>").text(firstName + " " + lastName);
+    var occupation2 = $("<p>").text(occupation);
+    var location2 = $("<p>").text(city + ", " + state);
+    $("#displayHere").empty();
+    $("#displayHere").append(name, occupation2, location2);
+
+  })
 
     // stop the default behavior of the submit button
 
