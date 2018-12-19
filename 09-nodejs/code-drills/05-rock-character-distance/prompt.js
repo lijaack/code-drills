d0a0e06be062224d2df21a46acecf4de75ac0afc
Jack <lijaack@yahoo.com>
2018-12-18 21:21:14 -0800

    code drill update node-05 done

--- 09-nodejs/code-drills/05-rock-character-distance/prompt.js
+++ 09-nodejs/code-drills/05-rock-character-distance/prompt.js
@@ -4,9 +4,30 @@
   
   function distanceFrom(str, char) {
 // ===================== YOUR WORK HERE =====================
+  var distance = [];
+  var indices = [];
+  // var indices = [2,6,13];
+  for (var i = 0; i < str.length; i++){
+    if (str[i] === char){
+      indices.push(i)
+    }
+  };
+
+  for (var j = 0; j < str.length; j++){
+    var z = str.length;
+
+    for(var x = 0; x < indices.length; x++){
+      y = Math.abs(j - indices[x]);
+      if (y < z){
+        z = y
+      }
+    }
+    distance.push(z);
+
+  }
 
 
-
+  return distance
 
 // ==========================================================
   }
