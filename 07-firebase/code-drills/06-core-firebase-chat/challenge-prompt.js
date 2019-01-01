0c253c2843428b318b9c769f4c39d74b69afeb02
Jack <lijaack@yahoo.com>
2018-12-31 23:04:12 -0800

    chat box code drill :)

--- 07-firebase/code-drills/06-core-firebase-chat/challenge-prompt.js
+++ 07-firebase/code-drills/06-core-firebase-chat/challenge-prompt.js
@@ -1,5 +1,15 @@
 // Firebase Chat
 
+var config = {
+  apiKey: "AIzaSyCWhpYJiH-C1_48_bexU8JGQn4AHn9ghkw",
+  authDomain: "project-ucb.firebaseapp.com",
+  databaseURL: "https://project-ucb.firebaseio.com",
+  projectId: "project-ucb",
+  storageBucket: "project-ucb.appspot.com",
+  messagingSenderId: "117886055106"
+};
+
+firebase.initializeApp(config);
 // creating a variable that references our database, so we don't have to type
 // it out every time
 var database = firebase.database();
@@ -9,8 +19,21 @@ $(function() {
 
   // -------------------- Your Code Here --------------------
 
+  database.ref().on("child_added", function (childSnapshot) {
+    var chat = $("<p>");
 
+    chat.text(childSnapshot.val());
+
+    $("#chat-display").append(chat);
+
+  });
   
+  $(document).on("click", "button[type=submit]", function (event){
+    event.preventDefault();
+    var chatMessage = $("input[type=text]").val().trim();
+    $("#chat-input").val("");
+    database.ref().push(chatMessage);
+  })
   
  
 
