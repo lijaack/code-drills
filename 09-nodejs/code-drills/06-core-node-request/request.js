310d43c8beb06b0ded00650bf6bcb67d2201a755
Jack <lijaack@yahoo.com>
2019-01-04 23:02:57 -0800

    Update request.js

--- 09-nodejs/code-drills/06-core-node-request/request.js
+++ 09-nodejs/code-drills/06-core-node-request/request.js
@@ -2,10 +2,9 @@ var request = require("request");
 
 var zip = process.argv[2];
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&zip="+zip+",us&APPID=f4a8c006b50a10720402ed8930a9f381";
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&zip="+zip+",us&APPID=444138542c9de2031e7cb5ad505031f6";
 
 request(queryURL, function (error, response, body) {
-    console.log(response)
     if (!error && response.statusCode === 200) {
 
       var forecast = JSON.parse(body).list;
