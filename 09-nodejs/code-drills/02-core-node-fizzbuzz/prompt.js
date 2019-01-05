c6640d9568f1ad17a180cf881726e5430071ec90
Jack <lijaack@yahoo.com>
2019-01-04 22:26:46 -0800

    node fizzbuzz codedrill

--- 09-nodejs/code-drills/02-core-node-fizzbuzz/prompt.js
+++ 09-nodejs/code-drills/02-core-node-fizzbuzz/prompt.js
@@ -1,8 +1,15 @@
 
 function fizzBuzz(n) {
 // ===================== YOUR WORK HERE =====================
+var number = process.argv[2]
 
-
+for (var i = 0; i < number; i++){
+    var output = "";
+    if (i % 3 === 0){output += "Fizz"};
+    if (i % 5 === 0){output += "Buzz"};
+    if (output === ""){output = i};
+    console.log(output);
+}
 
 
 // ==========================================================
