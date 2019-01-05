ff1bb57ab9d087c5cd1095ee77546ec35bfa5cef
Jack <lijaack@yahoo.com>
2019-01-04 22:33:55 -0800

    node quotes codedrill

--- 09-nodejs/code-drills/03-core-node-quotes/prompt.js
+++ 09-nodejs/code-drills/03-core-node-quotes/prompt.js
@@ -1,1 +1,25 @@
 // ======== You are given NOTHING to start with 😱 ========
+var fs = require("fs");
+
+var args = [];
+
+for (var i = 2 ; i < process.argv.length ; i++) {
+    args.push(process.argv[i]);
+}
+  
+var person = args.join(" ");
+
+fs.readFile("quotes.txt", "utf8", function(error, data) {
+  
+    var fullQuotes = data.split(";");
+  
+    for (var i = 0 ; i < fullQuotes.length; i++) {
+      var quote = fullQuotes[i].split("-");
+  
+      if (quote[1].toLocaleLowerCase() === person) {
+  
+        console.log(quote[0])
+  
+      }
+    }
+  });
